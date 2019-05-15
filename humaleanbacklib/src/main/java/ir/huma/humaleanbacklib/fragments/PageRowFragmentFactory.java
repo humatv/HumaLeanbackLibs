86a3c4ae4bd05a5f190d0d4c3d693244795b7dec
Hamed <hamedgramzi@gmail.com>
2019-05-15 15:19:27 +0430

    add fragmentfactory selected listener

--- humaleanbacklib/src/main/java/ir/huma/humaleanbacklib/fragments/PageRowFragmentFactory.java
+++ humaleanbacklib/src/main/java/ir/huma/humaleanbacklib/fragments/PageRowFragmentFactory.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v17.leanback.app.BackgroundManager;
 import android.support.v17.leanback.app.BrowseSupportFragment;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
 import android.support.v17.leanback.widget.Row;
 import android.support.v4.app.Fragment;
 
@@ -12,11 +13,12 @@ import java.util.HashMap;
 import java.util.List;
 
 public class PageRowFragmentFactory extends BrowseSupportFragment.FragmentFactory {
-//    private final BackgroundManager mBackgroundManager;
+    //    private final BackgroundManager mBackgroundManager;
     private Context context;
 
     HashMap<Long, Fragment> fragmentHashMap = new HashMap<>();
-
+    ArrayObjectAdapter adapter;
+    FragmentProvider provider;
 
 //    PageRowFragmentFactory(BackgroundManager backgroundManager, Context context) {
 //        this.mBackgroundManager = backgroundManager;
@@ -24,14 +26,25 @@ public class PageRowFragmentFactory extends BrowseSupportFragment.FragmentFactor
 //    }
 
 
-    public PageRowFragmentFactory(Context context) {
+    public PageRowFragmentFactory(Context context, ArrayObjectAdapter adapter, FragmentProvider provider) {
         this.context = context;
+        this.adapter = adapter;
+        this.provider = provider;
     }
 
     @Override
     public Fragment createFragment(Object rowObj) {
 //        mBackgroundManager.setDrawable(context.getResources().getDrawable(R.drawable.background));
         Row row = (Row) rowObj;
+        try {
+            for (int i = 0; i < adapter.size(); i++) {
+                if (adapter.get(i) == row) {
+                    provider.onItemSelectedListener(null, row, i, 0);
+                }
+            }
+        } catch (Exception e) {
+
+        }
 
         if (fragmentHashMap.containsKey(row.getHeaderItem().getId())) {
             return fragmentHashMap.get(row.getHeaderItem().getId());
