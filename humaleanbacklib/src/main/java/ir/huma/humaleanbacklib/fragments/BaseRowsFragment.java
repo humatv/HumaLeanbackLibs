8cd8ac49649d7173a585960434b92ec46106157f
Hamed <hamedgramzi@gmail.com>
2020-04-25 12:25:13 +0430

    add progress to baseRows

--- humaleanbacklib/src/main/java/ir/huma/humaleanbacklib/fragments/BaseRowsFragment.java
+++ humaleanbacklib/src/main/java/ir/huma/humaleanbacklib/fragments/BaseRowsFragment.java
@@ -1,6 +1,12 @@
 package ir.huma.humaleanbacklib.fragments;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
 import androidx.leanback.app.RowsSupportFragment;
 import androidx.leanback.widget.BaseOnItemViewClickedListener;
 import androidx.leanback.widget.BaseOnItemViewSelectedListener;
@@ -62,4 +68,29 @@ public abstract class BaseRowsFragment extends RowsSupportFragment implements Fr
             }
         });
     }
+
+    private ProgressBar progress;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = super.onCreateView(inflater, container, savedInstanceState);
+        RelativeLayout f = new RelativeLayout(getContext());
+        f.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        f.addView(v);
+        f.addView(getProgress());
+        return f;
+    }
+
+    public ProgressBar getProgress(){
+        if(progress == null ){
+            progress = new ProgressBar(getContext());
+            progress.setVisibility(View.GONE);
+            RelativeLayout.LayoutParams progressParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            progressParams.addRule(RelativeLayout.CENTER_IN_PARENT, 1);
+            progress.setLayoutParams(progressParams);
+        }
+        return progress;
+    }
+
+
 }
